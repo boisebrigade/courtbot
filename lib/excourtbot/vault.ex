5565894b31ea92ae6b5d712cfce0a8d8b8996d2d
Tyler Samples <samples.tm@gmail.com>
2018-09-10 15:21:31 -0600

    Check if ciphers are defined in config before trying to use env

--- lib/excourtbot/vault.ex
+++ lib/excourtbot/vault.ex
@@ -3,20 +3,24 @@ defmodule ExCourtbot.Vault do
   @impl Cloak.Vault
 
   def init(config) do
-    cloak_encryption_key =
-      System.get_env("CLOAK_ENCRYPTION_KEY") |> Base.decode64!() ||
-        raise "expected the CLOAK_ENCRYPTION_KEY environment variable to be set"
-
     config =
-      config
-      |> Keyword.put(:ciphers,
-        default: {Cloak.Ciphers.AES.GCM, tag: "AES.GCM.V1", key: cloak_encryption_key}
-      )
+      if config[:ciphers] do
+        config
+      else
+        cloak_encryption_key =
+          System.get_env("CLOAK_ENCRYPTION_KEY") |> Base.decode64!() ||
+            raise "expected the CLOAK_ENCRYPTION_KEY environment variable to be set"
+
+        config
+        |> Keyword.put(
+          :ciphers,
+          default: {Cloak.Ciphers.AES.GCM, tag: "AES.GCM.V1", key: cloak_encryption_key}
+        )
+      end
 
     {:ok, config}
   end
 end
-
 
 defmodule ExCourtbot.Encrypted.Binary do
   use Cloak.Fields.Binary, vault: ExCourtbot.Vault
