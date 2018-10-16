80ef0f2663cd342f5b34e1f13b6bd8ac6368d5ae
Tyler Samples <samples.tm@gmail.com>
2018-10-15 19:55:01 -0600

    Adding plug error handler

--- lib/excourtbot_web/router.ex
+++ lib/excourtbot_web/router.ex
@@ -1,5 +1,6 @@
 defmodule ExCourtbotWeb.Router do
   use ExCourtbotWeb, :router
+  use Plug.ErrorHandler
 
   pipeline :twilio do
     plug(:fetch_session)
@@ -33,5 +34,31 @@ defmodule ExCourtbotWeb.Router do
 
   scope "/", ExCourtbotWeb do
     forward("/", StaticPlug)
+  end
+
+  def handle_errors(conn, %{kind: kind, reason: reason, stack: stacktrace}) do
+    conn =
+      conn
+      |> Plug.Conn.fetch_cookies()
+      |> Plug.Conn.fetch_query_params()
+
+    params =
+      case conn.params do
+        %Plug.Conn.Unfetched{aspect: :params} -> "unfetched"
+        other -> other
+      end
+
+    occurrence_data = %{
+      "request" => %{
+        "cookies" => conn.req_cookies,
+        "url" => "#{conn.scheme}://#{conn.host}:#{conn.port}#{conn.request_path}",
+        "user_ip" => List.to_string(:inet.ntoa(conn.remote_ip)),
+        "headers" => Enum.into(conn.req_headers, %{}),
+        "method" => conn.method,
+        "params" => params,
+      }
+    }
+
+    Rollbax.report(kind, reason, stacktrace, _custom_data = %{}, occurrence_data)
   end
 end
