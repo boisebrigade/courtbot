3c979f24b41d36c13a34707d8244631e4eb5d986
Tyler Samples <samples.tm@gmail.com>
2018-12-23 15:26:26 -0700

    Make the admin reset password command generate a random password

--- lib/mix/tasks/courtbot.user.password.ex
+++ lib/mix/tasks/courtbot.user.password.ex
@@ -3,11 +3,13 @@ defmodule Mix.Tasks.Courtbot.User.Password do
 
   alias Courtbot.User
 
+  @random_passphase_length 32
+
   @shortdoc "Reset's Courtbot's admin users password"
   def run(_) do
     Mix.Task.run("app.start", [])
 
-    password = "abcdefghijk"
+    password = :crypto.strong_rand_bytes(@random_passphase_length) |> Base.encode64 |> binary_part(0, @random_passphase_length)
 
     User.reset_password("admin", password)
 
