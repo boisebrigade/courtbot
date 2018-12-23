cb4cbb9c42d6f3edd37b766e63e0a35d2e07566c
Tyler Samples <samples.tm@gmail.com>
2018-12-23 15:20:38 -0700

    Generate schema on mix setup

--- mix.exs
+++ mix.exs
@@ -71,10 +71,11 @@ defmodule Courtbot.Mixfile do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["ecto.drop", "deps.get", "ecto.setup", &setup_npm/1],
+      setup: ["ecto.drop", "deps.get", "ecto.setup", "schema", &setup_npm/1],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"],
+      schema: ["absinthe.schema.json --schema Courtbot.Schema assets/graphql_schema.json"]
     ]
   end
 
