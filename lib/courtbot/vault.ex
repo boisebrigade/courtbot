6998394d28dbf8d75adb65ff8adf2082d514a83f
Tyler Samples <samples.tm@gmail.com>
2019-02-13 22:41:25 -0700

    Deleting rollbar config and refactoring vault

--- lib/courtbot/vault.ex
+++ lib/courtbot/vault.ex
@@ -1,25 +1,5 @@
 defmodule Courtbot.Vault do
   use Cloak.Vault, otp_app: :courtbot
-  @impl Cloak.Vault
-
-  def init(config) do
-    config =
-      if config[:ciphers] do
-        config
-      else
-        cloak_encryption_key =
-          System.get_env("CLOAK_ENCRYPTION_KEY") |> Base.decode64!() ||
-            raise "expected the CLOAK_ENCRYPTION_KEY environment variable to be set"
-
-        config
-        |> Keyword.put(
-          :ciphers,
-          default: {Cloak.Ciphers.AES.GCM, tag: "AES.GCM.V1", key: cloak_encryption_key}
-        )
-      end
-
-    {:ok, config}
-  end
 end
 
 defmodule Courtbot.Encrypted.Binary do
