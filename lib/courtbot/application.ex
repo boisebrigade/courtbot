e171c2d38bc0a7976beb21e2deafe98baa8013cf
Tyler Samples <samples.tm@gmail.com>
2019-02-13 22:45:46 -0700

    Refactor how Courtbot bootstraps

--- lib/courtbot/application.ex
+++ lib/courtbot/application.ex
@@ -4,35 +4,15 @@ defmodule Courtbot.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec
-
     # Define workers and child supervisors to be supervised
     children = [
-      # Start the Ecto repository
-      supervisor(Courtbot.Repo, []),
-      # Start the endpoint when the application starts
-      supervisor(CourtbotWeb.Endpoint, [])
+      CourtbotWeb.Endpoint,
+      Courtbot.Repo,
+      {DynamicSupervisor, name: Courtbot.Bootstrap, strategy: :one_for_one},
     ]
 
-    # TODO(ts): Add rollbax and ex_twilio if we have their configuration
-
-    # Grab our scheduled times and append them to our children if they are defined.
-    scheduled =
-      Application.get_env(:courtbot, Courtbot, %{})
-      |> Map.new()
-      |> Map.take([:import_time, :notify_time])
-      |> Enum.map(fn
-        {:import_time, import_time} ->
-          %{id: "import", start: {SchedEx, :run_every, [Courtbot, :import, [], import_time]}}
-
-        {:notify_time, notify_time} ->
-          %{id: "notify", start: {SchedEx, :run_every, [Courtbot, :notify, [], notify_time]}}
-      end)
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Courtbot.Supervisor]
-    Supervisor.start_link(children ++ scheduled, opts)
+    Supervisor.start_link(children, opts)
   end
 
   # Tell Phoenix to update the endpoint configuration
--- lib/courtbot/repo.ex
+++ lib/courtbot/repo.ex
@@ -3,23 +3,10 @@ defmodule Courtbot.Repo do
     otp_app: :courtbot,
     adapter: Ecto.Adapters.Postgres
 
-  @doc """
-  Dynamically loads the repository url from the
-  DATABASE_URL environment variable.
-  """
   def init(_, config) do
-    if config[:load_from_system_env] do
-      database_url =
-        System.get_env("DATABASE_URL") ||
-          raise "expected the DATABASE_URL environment variable to be set"
-
-      config =
-        config
-        |> Keyword.put(:url, database_url)
-
-      {:ok, config}
-    else
-      {:ok, config}
+    case System.get_env("DATABASE_URL") do
+      nil -> {:ok, config}
+      database_url -> {:ok, Keyword.put(config, :url, database_url)}
     end
   end
 end
