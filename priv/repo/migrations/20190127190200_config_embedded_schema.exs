17c0833348cb952be8a7c541f3309848263f0fbc
Tyler Samples <samples.tm@gmail.com>
2019-02-13 22:54:50 -0700

    Add migration for configuration changes and removing importer table

---
+++ priv/repo/migrations/20190127190200_config_embedded_schema.exs
@@ -0,0 +1,22 @@
+defmodule Courtbot.Repo.Migrations.ConfigEmbeddedSchema do
+  use Ecto.Migration
+
+  def change do
+    alter table(:configuration) do
+      remove :id
+      remove :name
+      remove :value
+
+      add :importer, :map, default: %{}
+      add :rollbar, :map, default: %{}
+      add :twilio, :map, default: %{}
+      add :scheduled, :map, default: %{}
+      add :notifications, :map, default: %{}
+      add :locales, :map, default: %{}
+      add :variables, {:array, :map}, default: []
+      add :types, {:array, :map}, default: []
+    end
+
+    drop table(:importer)
+  end
+end
