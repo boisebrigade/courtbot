fc8d06b2727343440caa28e99c939bc7e49ed12f
Tyler Samples <samples.tm@gmail.com>
2019-02-15 23:56:50 -0700

    Add dynamic supervision for scheduled tasks

--- lib/courtbot/application.ex
+++ lib/courtbot/application.ex
@@ -1,6 +1,14 @@
 defmodule Courtbot.Application do
   use Application
 
+  alias Courtbot.Configuration.{
+    Rollbar,
+    Scheduled,
+    Scheduled.Tasks
+  }
+
+  require Logger
+
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
@@ -8,7 +16,9 @@ defmodule Courtbot.Application do
     children = [
       CourtbotWeb.Endpoint,
       Courtbot.Repo,
+      {Registry, keys: :unique, name: DynamicRegistry},
       {DynamicSupervisor, name: Courtbot.Bootstrap, strategy: :one_for_one},
+      Courtbot.ConfigMonitor
     ]
 
     opts = [strategy: :one_for_one, name: Courtbot.Supervisor]
@@ -20,5 +30,50 @@ defmodule Courtbot.Application do
   def config_change(changed, _new, removed) do
     CourtbotWeb.Endpoint.config_change(changed, removed)
     :ok
+  end
+
+  def stop_scheduled_tasks(scheduled) do
+    with %Scheduled{tasks: tasks} when tasks != [] <- scheduled do
+      Enum.map(tasks, fn %Tasks{name: name} ->
+        Logger.info("Stopping #{}  scheduled task")
+        cancel_scheduled_item(name)
+      end)
+    end
+  end
+
+  def start_scheduled_tasks(scheduled) do
+    with %Scheduled{tasks: tasks} when tasks != [] <- scheduled do
+      tasks
+      |> Enum.map(&child_spec_for_scheduled_task/1)
+      |> Enum.map(fn {name, childspec} ->
+        Logger.info("Starting #{name} scheduled task")
+
+        DynamicSupervisor.start_child(Courtbot.Bootstrap, childspec)
+      end)
+    end
+  end
+
+  def get_scheduled_item(id) do
+    list = Registry.lookup(RegistryName, id)
+
+    if length(list) > 0 do
+      {pid, _} = hd(list)
+      {:ok, pid}
+    else
+      {:error, "does not exist"}
+    end
+  end
+
+  def cancel_scheduled_item(id) do
+    with {:ok, pid} <- get_scheduled_item(id) do
+      DynamicSupervisor.terminate_child(Courtbot.Bootstrap, pid)
+    end
+  end
+
+  defp mfa_for_task(type) when type === "notify", do: [Courtbot.Notify, :run, []]
+  defp mfa_for_task(type) when type === "import", do: [Courtbot.Import, :run, []]
+
+  defp child_spec_for_scheduled_task(%Scheduled.Tasks{name: name, crontab: crontab}) do
+    {name, %{id: "scheduled-task-#{name}", start: {SchedEx, :run_every, mfa_for_task(name) ++ [crontab, [name: {:via, Registry, {DynamicRegistry, "scheduled-task-#{name}"}}]]}}}
   end
 end
---
+++ lib/courtbot/config_monitor.ex
@@ -0,0 +1,58 @@
+defmodule Courtbot.ConfigMonitor do
+  use GenServer
+
+  @moduledoc false
+
+  alias Courtbot.{
+    Application,
+    Configuration
+  }
+  alias Courtbot.Configuration.{
+    Rollbar,
+    Scheduled
+  }
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  @impl true
+  def init(_) do
+    {:ok, %{}, {:continue, :init}}
+  end
+
+  @impl true
+  def handle_continue(:init, state) do
+    schedule_check(1_000)
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info(:check, state) do
+
+    config = with config when not is_nil(config) <- Configuration.get([:scheduled, :rollbar]) do
+      if Map.equal?(state, config) do
+        schedule_check(60_000)
+
+        {:noreply, config}
+
+      else
+        schedule_check(60_000)
+
+        with %{scheduled: previous_scheduled} <- state do
+          Application.stop_scheduled_tasks(previous_scheduled)
+        end
+
+        Application.start_scheduled_tasks(scheduled)
+      end
+
+      config
+    end
+
+    {:noreply, config}
+  end
+
+  defp schedule_check(interval) do
+    Process.send_after(self(), :check, interval)
+  end
+end
