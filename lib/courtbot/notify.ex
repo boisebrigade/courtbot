1653731fafc2fd58ab8053772e94d2676c4b2d61
Tyler Samples <samples.tm@gmail.com>
2019-02-25 21:40:56 -0700

    Mix format and fixing utc timezone bug

--- lib/courtbot/notify.ex
+++ lib/courtbot/notify.ex
@@ -22,43 +22,67 @@ defmodule Courtbot.Notify do
       locales: locales,
       notifications: %{reminders: reminders},
       twilio: twilio,
-      timezone: timezone,
+      timezone: timezone
     } = Configuration.get([:locales, :notifications, :twilio, :timezone])
 
-    Enum.map(reminders, &(Enum.each(&1, fn({timescale, offset}) ->
-      date = Timex.shift(Date.utc_today(), [{String.to_atom(timescale), offset}])
+    Enum.map(
+      reminders,
+      &Enum.each(&1, fn {timescale, offset} ->
+        day = get_day(timezone, [{String.to_atom(timescale), offset}])
 
-      with pending when pending != [] <- notifications_for_day(day, timezone) do
-        pending
-        |> Enum.chunk_every(100)
-        |> Enum.with_index()
-        |> Enum.map(fn {notifications, batch} ->
-          SchedEx.run_at(Courtbot.Notify, :notify_subscriber_list, [notifications, locales, twilio], Timex.shift(DateTime.utc_now(), seconds: 100 * batch))
-        end)
-      end
-    end)))
+        with pending when pending != [] <- notifications_for_day(day) do
+          pending
+          |> Enum.chunk_every(100)
+          |> Enum.with_index()
+          |> Enum.map(fn {notifications, batch} ->
+            SchedEx.run_at(
+              Courtbot.Notify,
+              :notify_subscriber_list,
+              [notifications, locales, twilio],
+              Timex.shift(DateTime.utc_now(), seconds: 100 * batch)
+            )
+          end)
+        end
+      end)
+    )
 
     notify_debug_subscribers(locales, twilio)
 
     Logger.info("Finished starting batches of notifications")
   end
 
-  def notify_subscriber_list(notifications, locales, twilio_credentials) do
-    Enum.each(notifications, fn %{case: case, phone_number: phone_number, id: subscriber_id, locale: locale} ->
-       twilio = Twilio.new(twilio_credentials)
-       from_number = Map.fetch!(locales, locale)
-       body = Response.get_message({:remind, case}, locale)
+  defp get_day(timezone, offset) do
+    {:ok, today} = DateTime.shift_zone(DateTime.utc_now(), timezone, Tzdata.TimeZoneDatabase)
+    Timex.shift(today, offset)
+  end
 
-       with {:ok, _result = %Tesla.Env{status: 201}} <- Twilio.message(twilio, %{From: from_number, To: phone_number, Body: body}) do
-         %Notification{}
-         |> Notification.changeset(%{subscriber_id: subscriber_id})
-         |> Repo.insert()
-       else
-         {:ok, %Tesla.Env{status: status, body: body}} ->
-           Logger.error("Unable to notify subscribers. Request to Twilio failed with #{status} and code #{body["code"]}")
-         {:error, _} ->
-           Logger.error("Unable to send request to Twilio to notify subscribers")
-       end
+  def notify_subscriber_list(notifications, locales, twilio_credentials) do
+    Enum.each(notifications, fn %{
+                                  case: case,
+                                  phone_number: phone_number,
+                                  id: subscriber_id,
+                                  locale: locale
+                                } ->
+      twilio = Twilio.new(twilio_credentials)
+      from_number = Map.fetch!(locales, locale)
+      body = Response.get_message({:remind, case}, locale)
+
+      with {:ok, _result = %Tesla.Env{status: 201}} <-
+             Twilio.message(twilio, %{From: from_number, To: phone_number, Body: body}) do
+        %Notification{}
+        |> Notification.changeset(%{subscriber_id: subscriber_id})
+        |> Repo.insert()
+      else
+        {:ok, %Tesla.Env{status: status, body: body}} ->
+          Logger.error(
+            "Unable to notify subscribers. Request to Twilio failed with #{status} and code #{
+              body["code"]
+            }"
+          )
+
+        {:error, _} ->
+          Logger.error("Unable to send request to Twilio to notify subscribers")
+      end
     end)
   end
 
@@ -66,25 +90,27 @@ defmodule Courtbot.Notify do
     Logger.info("Sending debug notifications")
 
     # Notify debug subscribers
-    %Case{id: case_id} = Case.find_with([case_number: "beepboop"])
+    %Case{id: case_id} = Case.find_with(case_number: "beepboop")
 
     Enum.map(Subscriber.subscribers_to_case(case_id), fn subscriber ->
-      Twilio.message(Twilio.new(twilio_credentials), %{From: Map.fetch!(locales, "en"), To: subscriber.phone_number, Body: "BEEPBOOP"})
+      Twilio.message(Twilio.new(twilio_credentials), %{
+        From: Map.fetch!(locales, "en"),
+        To: subscriber.phone_number,
+        Body: "BEEPBOOP"
+      })
     end)
   end
 
-  def notifications_for_day(day, timezone) do
-    {:ok, today} = DateTime.shift_zone(day, timezone, Tzdata.TimeZoneDatabase)
-
+  def notifications_for_day(day) do
     notified =
       from(
         n in Notification,
-        where: n.inserted_at >= ^Timex.beginning_of_day(today),
-        where: n.inserted_at <= ^Timex.end_of_day(today),
+        where: n.inserted_at >= ^Timex.beginning_of_day(day),
+        where: n.inserted_at <= ^Timex.end_of_day(day),
         select: n.subscriber_id
       )
 
-    %Case{id: debug_case_id} = Case.find_with([case_number: "beepboop"])
+    %Case{id: debug_case_id} = Case.find_with(case_number: "beepboop")
 
     from(
       s in Subscriber,
@@ -98,16 +124,16 @@ defmodule Courtbot.Notify do
       where: h.date == ^day,
       preload: [
         case: [
-          hearings: ^from(
-            h in Hearing,
-            order_by: [h.date, h.time],
-            where: h.date >= ^today,
-            limit: 1
-          )
+          hearings:
+            ^from(
+              h in Hearing,
+              order_by: [h.date, h.time],
+              where: h.date >= ^day,
+              limit: 1
+            )
         ]
       ]
     )
     |> Repo.all()
   end
-
 end
