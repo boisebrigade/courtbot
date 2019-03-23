6c75a3b6b29d08503fd64c1c19982b8919e04d4a
Tyler Samples <samples.tm@gmail.com>
2019-03-23 10:42:23 -0600

    Only report 500 errors and unknown errors to rollbar

--- lib/courtbot_web/router.ex
+++ lib/courtbot_web/router.ex
@@ -14,28 +14,33 @@ defmodule CourtbotWeb.Router do
   end
 
   def handle_errors(conn, %{kind: kind, reason: reason, stack: stacktrace}) do
-    conn =
-      conn
-      |> Plug.Conn.fetch_cookies()
-      |> Plug.Conn.fetch_query_params()
+    if report?(kind, reason) do
+      conn =
+        conn
+        |> Plug.Conn.fetch_cookies()
+        |> Plug.Conn.fetch_query_params()
 
-    params =
-      case conn.params do
-        %Plug.Conn.Unfetched{aspect: :params} -> "unfetched"
-        other -> other
-      end
+      params =
+        case conn.params do
+          %Plug.Conn.Unfetched{aspect: :params} -> "unfetched"
+          other -> other
+        end
 
-    occurrence_data = %{
-      "request" => %{
-        "cookies" => conn.req_cookies,
-        "url" => "#{conn.scheme}://#{conn.host}:#{conn.port}#{conn.request_path}",
-        "user_ip" => List.to_string(:inet.ntoa(conn.remote_ip)),
-        "headers" => Enum.into(conn.req_headers, %{}),
-        "method" => conn.method,
-        "params" => params
+      occurrence_data = %{
+        "request" => %{
+          "cookies" => conn.req_cookies,
+          "url" => "#{conn.scheme}://#{conn.host}:#{conn.port}#{conn.request_path}",
+          "user_ip" => List.to_string(:inet.ntoa(conn.remote_ip)),
+          "headers" => Enum.into(conn.req_headers, %{}),
+          "method" => conn.method,
+          "params" => params
+        }
       }
-    }
 
-    Rollbax.report(kind, reason, stacktrace, _custom_data = %{}, occurrence_data)
+      Rollbax.report(kind, reason, stacktrace, _custom_data = %{}, occurrence_data)
+    end
   end
+
+  defp report?(:error, exception), do: Plug.Exception.status(exception) == 500
+  defp report?(_kind, _reason), do: true
 end
