ffbaab9228c29e094a464c72b2eac5adac4a7395
blakedietz <blakedietz@gmail.com>
2020-04-11 18:58:31 -0600

    Update setup script to include self signed cert generation

--- mix.exs
+++ mix.exs
@@ -74,7 +74,7 @@ defmodule Courtbot.Mixfile do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup"],
+      setup: ["deps.get", "ecto.setup", "phx.gen.cert selfsigned"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"]
